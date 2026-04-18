755019161b737727b12adf33a9fac100fb32b630
shakib-2020 <nazshakib01@gmail.com>
2026-04-19 02:06:30 +0600

    Refactor code structure for improved readability and maintainability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 ﻿// src/app/layout.tsx
-'use client';
 
 import type { Metadata } from 'next'
 import './globals.css'
@@ -7,26 +6,34 @@ import Header from '@/components/layout/Header'
 import Footer from '@/components/layout/Footer'
 import StoreProvider from '@/providers/StoreProvider'
 import NextAuthSessionProvider from "../providers/NextAuthSessionProvider"
-import { usePathname } from 'next/navigation'
 import { Toaster } from 'react-hot-toast';
+
+export const metadata: Metadata = {
+  title: "The Car Edition Pro",
+  description: "The Car Edition Pro is your trusted partner for professional automotive services. We deliver quality maintenance, repairs, and diagnostics with expert care and attention to detail.",
+  icons: {
+    icon: "/icon.jpeg"
+  },
+  authors: [
+    {
+      name: "The Car Edition Pro",
+      url: "https://thecaredition.co.uk"
+    }
+  ],
+  keywords: [
+    "car repair, automotive services, engine rebuilds, timing chains, turbos, brakes and pads, diagnostics, maintenance"],
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = usePathname();
-  const isDashboard = pathname?.startsWith('/dashboard');
 
   return (
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>The Car Edition Pro</title>
-        <meta name="description" content="The Car Edition Pro is your trusted partner for professional automotive services. We deliver quality maintenance, repairs, and diagnostics with expert care and attention to detail." />
-        <meta name="keywords" content="car repair, automotive services, engine rebuilds, timing chains, turbos, brakes and pads, diagnostics, maintenance" />
-        <meta name="author" content="The Car Edition Pro" />
-        <link rel="icon" href="/favicon.ico" />
       </head>
       <NextAuthSessionProvider>
         <StoreProvider>
